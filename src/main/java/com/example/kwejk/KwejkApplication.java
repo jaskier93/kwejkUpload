88d677491f97adfa1187ebb305c09b919906287b
Damian Grochowski <smg7@o2.pl>
2019-04-11 18:38:22 +0200

    updated

--- src/main/java/com/example/kwejk/KwejkApplication.java
+++ src/main/java/com/example/kwejk/KwejkApplication.java
@@ -1,13 +1,28 @@
 package com.example.kwejk;
 
+import com.example.kwejk.upload.StorageProperties;
+import com.example.kwejk.upload.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class KwejkApplication {
+    @EnableConfigurationProperties(StorageProperties.class)
+    public class KwejkApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(KwejkApplication.class, args);
+        public static void main(String[] args) {
+            SpringApplication.run(KwejkApplication.class, args);
+        }
+
+        @Bean
+        CommandLineRunner init(StorageService storageService) {
+            return (args) -> {
+                storageService.deleteAll();
+                storageService.init();
+            };
+        }
     }
 
-}
+
